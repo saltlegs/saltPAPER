96638d8885866f6cb1b8e40691ca1789a1b80855
Lauren Kinder <margidinium@gmail.com>
2026-03-15 23:53:03 +0000

    add setter to entity

--- saltpaper/worldsystem/entity.py
+++ saltpaper/worldsystem/entity.py
@@ -34,9 +34,18 @@ class Entity():
     def kill(self):
         self.killed = True
         self.world.entities.pop(self.id, None)
+        return self
 
     def __getattr__(self, name):
         for component in self.components.values():
             if hasattr(component, name):
                 return getattr(component, name)
         raise AttributeError(f"{type(self).__name__} has no attribute '{name}'")
+
+    def __setattr__(self, name, value):
+        if "components" in self.__dict__:
+            for component in self.components.values():
+                if hasattr(component, name):
+                    setattr(component, name, value)
+                    return
+        object.__setattr__(self, name, value)
